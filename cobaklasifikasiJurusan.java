77ca53353b47625193cb9fac3e3a52495e8f887c
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2021-02-23 16:33:38 +0700

    Delete cobaklasifikasiJurusan.java

--- cobaklasifikasiJurusan.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class cobaklasifikasiJurusan {
-    public static void main(String[] args) {
-        String nama = "T01679";
-        System.out.println(nama.substring(1,3));
-
-    }
-}
